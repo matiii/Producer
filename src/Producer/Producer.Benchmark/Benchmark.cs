290af6b43d0716bd5441ba6aa4aa0ccc8d7946d7
Mateusz Mazurek <mateusz.mazurek@gmail.com>
2016-10-26 13:10:39 +0200

    benchmark

---
+++ src/Producer/Producer.Benchmark/Benchmark.cs
@@ -0,0 +1,39 @@
+﻿namespace Producer.Benchmark
+{
+    using BenchmarkDotNet.Attributes;
+    using System;
+    using Tests.Model;
+
+    public class Benchmark
+    {
+
+        private const int Length = 100000;
+
+        private readonly object[] _args = { "", 5, (long) 6, 2.2, true, ""};
+
+        [Benchmark]
+        public Foo[] ActivatorCreateInstance()
+        {
+            var foos = new Foo[Length];
+
+            for (int i = 0; i < Length; i++)
+                foos[i] = (Foo) Activator.CreateInstance(typeof(Foo), _args);
+
+            return foos;
+        }
+
+        [Benchmark]
+        public Foo[] ProducerProduce()
+        {
+            var foos = new Foo[Length];
+
+            var f = Producer.Produce<Foo>(typeof(string), typeof(int), typeof(long), typeof(double),
+                typeof(bool), typeof(object));
+
+            for (int i = 0; i < Length; i++)
+                foos[i] = f(_args);
+
+            return foos;
+        }
+    }
+}
---
+++ src/Producer/Producer.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Producer.Benchmark
+{
+    using BenchmarkDotNet.Running;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
